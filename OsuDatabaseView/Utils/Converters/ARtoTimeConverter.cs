a42a7d817f1e4b1e0724a63fc368acdcc1ad77a1
Kamil Szabłowski <147844276+aulunax@users.noreply.github.com>
2024-09-28 20:23:28 +0200

    Tooltips for AR and OD ms exact values

---
+++ OsuDatabaseView/Utils/Converters/ARtoTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using OsuDatabaseControl.Common;
+
+namespace OsuDatabaseView.Utils.Converters
+{
+    public class ARtoTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float ar)
+            {
+                return $"{DifficultyValueConverter.GetApproachRateValue(ar)}ms";
+            }
+            return "?";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OsuDatabaseView/Utils/Converters/ODtoHitWindowConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using OsuDatabaseControl.Common;
+
+namespace OsuDatabaseView.Utils.Converters
+{
+    public class ODtoHitWindowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float od)
+            {
+                return $"300: {DifficultyValueConverter.GetOverallDifficultyValue(od)}ms";
+            }
+            return "300: ?";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
